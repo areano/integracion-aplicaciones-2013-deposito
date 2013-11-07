7ef497465af41fb70defe4fd45b277f67bddb19c
jair.tabares@gmail.com <jair.tabares@gmail.com@1e37a482-7787-480a-1d71-fe0db277f6fa>
2013-11-07 01:04:13 +0000

    new definition for DTO's by creating common superclass for articles

---
+++ trunk/Deposito_EJB/ejbModule/dto/ArticuloDTO.java
@@ -0,0 +1,82 @@
+package dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotNull;
+
+public abstract class ArticuloDTO implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long codigo;
+	@NotNull
+	private String nombre;
+	@NotNull
+	private Long codigoDeposito;
+	@NotNull
+	private String descripcion;
+	@NotNull
+	private String marca;
+	@NotNull
+	private Float precio;
+	@NotNull
+	private String origen;
+	@NotNull
+	private String foto;	
+
+	public ArticuloDTO(){
+	    codigo=null;
+	    descripcion=null;
+	    foto = null;
+	    marca=null;
+	    nombre=null;
+	    precio=null;
+	    codigoDeposito=null;
+	}
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Float precio) {
+		this.precio = precio;
+	}
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+	
+	
+}
--- trunk/Deposito_EJB/ejbModule/dto/MuebleDTO.java
+++ trunk/Deposito_EJB/ejbModule/dto/MuebleDTO.java
@@ -1,82 +1,20 @@
 package dto;
 
-public class MuebleDTO {
+import javax.validation.constraints.NotNull;
+
+public class MuebleDTO extends ArticuloDTO{
 	
 
-	private long codigo;
-	private String nombre;
-	private long codigoDeposito;
-	private String descripcion;
-	private String marca;
-	private float precio;
-	private String origen;
-	private String foto;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4347083384610652409L;
+	@NotNull
 	private String material;
 	
-	public MuebleDTO(){}
-
-	public long getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(long codigo) {
-		this.codigo = codigo;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public long getCodigoDeposito() {
-		return codigoDeposito;
-	}
-
-	public void setCodigoDeposito(long codigoDeposito) {
-		this.codigoDeposito = codigoDeposito;
-	}
-
-	public String getDescripcion() {
-		return descripcion;
-	}
-
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-
-	public String getMarca() {
-		return marca;
-	}
-
-	public void setMarca(String marca) {
-		this.marca = marca;
-	}
-
-	public float getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(float precio) {
-		this.precio = precio;
-	}
-
-	public String getOrigen() {
-		return origen;
-	}
-
-	public void setOrigen(String origen) {
-		this.origen = origen;
-	}
-
-	public String getFoto() {
-		return foto;
-	}
-
-	public void setFoto(String foto) {
-		this.foto = foto;
+	public MuebleDTO(){
+		super();
+		material = null;
 	}
 
 	public String getMaterial() {
