fb6c60c6d833fa41f9f47ab95043d37600781166
areanho@gmail.com <areanho@gmail.com@1e37a482-7787-480a-1d71-fe0db277f6fa>
2013-11-09 19:10:59 +0000

    * solucionando warnings

--- Deposito_EJB/ejbModule/dto/SolicitudArticuloItemDTO.java
+++ Deposito_EJB/ejbModule/dto/SolicitudArticuloItemDTO.java
@@ -1,35 +1,33 @@
 package dto;
 
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlSeeAlso;
-import javax.xml.bind.annotation.XmlType;
 
 @XmlRootElement(name = "articulo")
 public class SolicitudArticuloItemDTO {
 	public SolicitudArticuloItemDTO() {
 		super();
 	}
+
 	String codigo;
 	Integer cantidad;
-	
+
 	@XmlElement
 	public String getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
-	
+
 	@XmlElement
 	public Integer getCantidad() {
 		return cantidad;
 	}
+
 	public void setCantidad(Integer cantidad) {
 		this.cantidad = cantidad;
 	}
-	
+
 }
-
-
--- Fabrica_WEB/src/rest/RecibirSolicitudCompraRest.java
+++ Fabrica_WEB/src/rest/RecibirSolicitudCompraRest.java
@@ -4,7 +4,6 @@ import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import parsers.ParserException;
