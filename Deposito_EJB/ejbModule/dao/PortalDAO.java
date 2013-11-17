4ffb7e50b530e9ac65a8ed4b33495c4f098e606b
areanho@gmail.com <areanho@gmail.com@1e37a482-7787-480a-1d71-fe0db277f6fa>
2013-11-17 22:44:07 +0000

    * contructor publico para ArticuloDAO * solucionados los errores en las consultas JPQL

--- Deposito_EJB/ejbModule/dao/PortalDAO.java
+++ Deposito_EJB/ejbModule/dao/PortalDAO.java
@@ -19,7 +19,6 @@ import dto.ModaDTO;
 import dto.MuebleDTO;
 import parsers.ArticuloParser;
 import transformer.Transformer;
-import entities.Articulo;
 import entities.Electrodomestico;
 import entities.Infantil;
 import entities.Moda;
@@ -37,47 +36,45 @@ public class PortalDAO {
 	EntityManager em;
 
 	private List<PortalConexion> conexiones;
-	private static final Logger logger = 
-			   Logger.getLogger(PortalDAO.class);
+	private static final Logger logger = Logger.getLogger(PortalDAO.class);
+
 	public PortalDAO() {
 		conexiones = new ArrayList<PortalConexion>();
-		this.obtenerConexiones();
 	}
 
 	@SuppressWarnings("unchecked")
 	private void obtenerConexiones() {
-		Query q = em.createQuery("select from PortalConexion and active = TRUE");
+		Query q = em.createQuery("from PortalConexion where active = TRUE");
 		conexiones = (List<PortalConexion>) q.getResultList();
 	}
 
-	
-	public void enviar (Mueble m){
+	public void enviar(Mueble m) {
 		ArticuloParser parser = new ArticuloParser();
 		MuebleDTO mDTO = Transformer.obtenerInstancia().toDTO(m);
-		String xml=parser.toXML(mDTO);
+		String xml = parser.toXML(mDTO);
 		enviar(xml);
 	}
 
-	public void enviar (Infantil i){
+	public void enviar(Infantil i) {
 		ArticuloParser parser = new ArticuloParser();
 		InfantilDTO mDTO = Transformer.obtenerInstancia().toDTO(i);
-		String xml=parser.toXML(mDTO);
+		String xml = parser.toXML(mDTO);
 		enviar(xml);
 	}
 
-	public void enviar (Electrodomestico e){
+	public void enviar(Electrodomestico e) {
 		ArticuloParser parser = new ArticuloParser();
 		ElectrodomesticoDTO eDTO = Transformer.obtenerInstancia().toDTO(e);
-		String xml=parser.toXML(eDTO);
-		enviar(xml);
-	}
-	public void enviar (Moda m){
-		ArticuloParser parser = new ArticuloParser();
-		ModaDTO mDTO = Transformer.obtenerInstancia().toDTO(m);
-		String xml=parser.toXML(mDTO);
+		String xml = parser.toXML(eDTO);
 		enviar(xml);
 	}
 
+	public void enviar(Moda m) {
+		ArticuloParser parser = new ArticuloParser();
+		ModaDTO mDTO = Transformer.obtenerInstancia().toDTO(m);
+		String xml = parser.toXML(mDTO);
+		enviar(xml);
+	}
 
 	public void enviar(String xml) {
 		this.obtenerConexiones();
@@ -88,7 +85,7 @@ public class PortalDAO {
 				cliente.enviar(xml);
 				cliente.cerrarConexion();
 			} catch (JMSException e) {
-				errorMessage = "*** Error enviando xml a jms de Portal IP["+p.getIp()+"] Grupo ["+p.getPortalId()+"]***"; 
+				errorMessage = "*** Error enviando xml a jms de Portal IP[" + p.getIp() + "] Grupo [" + p.getPortalId() + "]***";
 				logger.error(errorMessage, e);
 			}
 		}
