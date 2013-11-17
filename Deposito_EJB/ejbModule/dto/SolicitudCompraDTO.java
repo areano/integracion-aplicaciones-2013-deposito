664d2ad79b5e9a2a633974ee62a3ef646d8aeda5
martin.flory@gmail.com <martin.flory@gmail.com@1e37a482-7787-480a-1d71-fe0db277f6fa>
2013-11-17 19:16:08 +0000

    Parser SolicitudCompra Deposito <-> Fabrica

--- Deposito_EJB/ejbModule/dto/SolicitudCompraDTO.java
+++ Deposito_EJB/ejbModule/dto/SolicitudCompraDTO.java
@@ -1,10 +1,20 @@
 package dto;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "solicitudCompra")
 public class SolicitudCompraDTO {
 
+	public SolicitudCompraDTO() {
+		super();
+		articulos = new ArrayList<ItemSolicitudCompraDTO>();
+	}
 	private long codigo;
 	private List<ItemSolicitudCompraDTO> articulos;
 	private Date fechaInicio;
@@ -16,18 +26,23 @@ public class SolicitudCompraDTO {
 	public void setCodigo(long codigo) {
 		this.codigo = codigo;
 	}
+	
+	@XmlElementWrapper(name = "articulos")
+	@XmlElement(name = "articulo")
 	public List<ItemSolicitudCompraDTO> getArticulos() {
 		return articulos;
 	}
 	public void setArticulos(List<ItemSolicitudCompraDTO> articulos) {
 		this.articulos = articulos;
 	}
+	
 	public Date getFechaInicio() {
 		return fechaInicio;
 	}
 	public void setFechaInicio(Date fechaInicio) {
 		this.fechaInicio = fechaInicio;
 	}
+	
 	public Date getFechaFin() {
 		return fechaFin;
 	}
