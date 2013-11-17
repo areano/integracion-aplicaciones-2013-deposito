6189bfdb5a780c0263093865f081420154dc9e07
martin.flory@gmail.com <martin.flory@gmail.com@1e37a482-7787-480a-1d71-fe0db277f6fa>
2013-11-17 19:58:37 +0000

    Reorganizado

---
+++ Fabrica_EJB/ejbModule/servicios/AdministradorSolicitudCompra.java
@@ -0,0 +1,14 @@
+package servicios;
+
+import javax.ejb.Local;
+
+import dto.SolicitudCompraDTO;
+
+@Local
+public interface AdministradorSolicitudCompra {
+
+	void recibirSolicitudCompra(SolicitudCompraDTO compra);
+
+	void entregarCompra(SolicitudCompraDTO compra);
+
+}
