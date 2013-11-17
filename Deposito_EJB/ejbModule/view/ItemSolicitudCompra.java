20dbf5d6c319ef7ee34ddee44e0c0171e5869950
jair.tabares <jair.tabares@1e37a482-7787-480a-1d71-fe0db277f6fa>
2013-11-17 21:39:58 +0000

    new view pkg

---
+++ Deposito_EJB/ejbModule/view/ItemSolicitudCompra.java
@@ -0,0 +1,25 @@
+package view;
+
+import dto.ArticuloDTO;
+
+public class ItemSolicitudCompra {
+	private ArticuloDTO articulo;
+	private int cantidad;
+	public ItemSolicitudCompra(ArticuloDTO articulo, int cantidad){
+		this.articulo = articulo;
+		this.cantidad = cantidad;		
+	}
+	public ArticuloDTO getArticulo() {
+		return articulo;
+	}
+	public void setArticulo(ArticuloDTO articulo) {
+		this.articulo = articulo;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+}
---
+++ Deposito_EJB/ejbModule/view/SolicitudArticulosView.java
@@ -0,0 +1,50 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class SolicitudArticulosView {
+	private ArrayList<SolicitudArticulosItemView> items = new ArrayList<SolicitudArticulosItemView>();
+	private int idModulo;
+	private Date date;
+	private long codigoSolicitud;
+
+	public void setItems(ArrayList<SolicitudArticulosItemView> items) {
+		this.items = items;
+	}
+	public void addItemSolicitudArticulos(SolicitudArticulosItemView item){
+		items.add(item);
+	}
+
+	public long getIdModulo() {
+		return idModulo;
+	}
+
+	public void setIdModulo(int idModulo) {
+		this.idModulo = idModulo;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public long getCodigoSolicitud() {
+		return codigoSolicitud;
+	}
+
+	public void setCodigoSolicitud(long codigoSolicitud) {
+		this.codigoSolicitud = codigoSolicitud;
+	}
+
+	public ArrayList<SolicitudArticulosItemView> getArticulos() {
+		return items;
+	}
+	public ArrayList<SolicitudArticulosItemView> getItems() {
+		return items;
+	}
+
+}
